7527183e97fb3776a5761219ec7f08378a0087a3
Bert Carapiet <bertman30@msn.com>
2015-10-13 14:58:44 -0400

    Added log file
    also changed some of the wording
    echo "`date`" " -- Fermez la porte (+1 hour after Sunset)
    
    and also changed the offset to -1:00:00 to see if that fixes the trigger from happening.  It would seem that a +0:30:00 offset triggers the activity to happen 30 minutes before the sunset activity.

--- check_for_sunset.sh
+++ check_for_sunset.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+log_file = "/home/pi/chicken_coop/check_door.log"
+
 #Eganville
 lat="45.5333N"
 lon="77.1000W"
@@ -7,12 +9,12 @@ lon="77.1000W"
 sunset_time=$(/home/pi/chicken_coop/sunwait08/sunwait/0.8/sunwait list $lat $lon | awk 'BEGIN {FS=",";} { print $2; }' )
 
 #Wait for the sunset, then close the door 30 minutes after 
-echo "`date`" " -- Waited for sunset starting at " $sunset_time >> /home/pi/chicken_coop/check_door.log
+echo "`date`" " -- Begun waiting for sunset " $sunset_time | cat - $log_file > temp && mv temp $log_file
 
-/home/pi/chicken_coop/sunwait08/sunwait/0.8/sunwait wait down offset 0:30:00 $lat $lon
+/home/pi/chicken_coop/sunwait08/sunwait/0.8/sunwait wait down offset -1:00:00 $lat $lon
 
 sudo python /home/pi/chicken_coop/relay_4port/close_in1.py
-echo "`date`" " -- Fermez la porte (30 minutes after Sunset)" >> /home/pi/chicken_coop/check_door.log
+echo "`date`" " -- Fermez la porte (+1 hour after Sunset)" | cat - $log_file > temp && mv temp $log_file
 
-mail -s "John and Alis Chicken Coop Door has been closed" bertman30@msn.com < /home/pi/chicken_coop/check_door.log
+mail -s "John and Alis Chicken Coop Door has been closed" bertman30@msn.com < $log_file
 
